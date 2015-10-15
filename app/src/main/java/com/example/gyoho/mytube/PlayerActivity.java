f7a7a43a60be509e9b4c13e22ced5cee382ee7eb
Yaopeng Wu <yaopeng.wu@sjsu.edu>
2015-10-15 16:32:52 -0700

    added search and play features

---
+++ app/src/main/java/com/example/gyoho/mytube/PlayerActivity.java
@@ -0,0 +1,41 @@
+package com.example.gyoho.mytube;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+/**
+ * Created by gyoho on 10/15/15.
+ */
+public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private YouTubePlayerView playerView;
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        setContentView(R.layout.activity_player);
+
+        playerView = (YouTubePlayerView)findViewById(R.id.player_view);
+        playerView.initialize(YoutubeConnector.KEY, this);
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult result) {
+        Toast.makeText(this, getString(R.string.failed), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+                                        boolean restored) {
+        if(!restored){
+            player.cueVideo(getIntent().getStringExtra("VIDEO_ID"));
+        }
+    }
+}
